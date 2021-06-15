9692c865ea054dd0799dc5c7544ea7effc8ab8a8
lucasMart27 <lucas_mart27@hotmail.com>
2021-06-14 23:55:17 -0300

    atualização

---
+++ CsvHelper _ASP.NET_Core/Controllers/StudentsController.cs
@@ -0,0 +1,74 @@
+﻿using CsvHelper;
+using CsvHelper__ASP.NET_Core.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsvHelper__ASP.NET_Core.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(List<Student> students = null)
+        {
+            students = students == null ? new List<Student>() : students;
+            return View(students);
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Index(IFormFile file, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            #region Upload CSV
+            string fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            #endregion
+
+            var students = this.GetStudentsList(file.FileName);
+            return Index(students);
+        }
+
+        private List<Student> GetStudentsList (string fileName)
+        {
+            List<Student> students = new List<Student>();
+
+            #region Read CSV
+            var path = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\files"}" + "\\" + fileName;
+            using (var reader = new StreamReader (path))
+            using (var csv = new CsvReader (reader, CultureInfo.InvariantCulture))
+            {
+                csv.Read();
+                csv.ReadHeader();
+                while(csv.Read())
+                {
+                    var student = csv.GetRecord<Student>();
+                    students.Add(student);
+                }
+            }
+
+            #endregion
+
+            #region Create CSV 
+            path = $"{Directory.GetCurrentDirectory()}{@"\wwwroot\FilesTo"}";
+            using (var write = new StreamWriter(path + "\\NewFile.csv"))
+                using (var csv = new CsvWriter(write, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(students);
+            }
+            #endregion
+
+            return students;
+        }
+    }
+}
---
+++ CsvHelper _ASP.NET_Core/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsvHelper__ASP.NET_Core.Models
+{
+    public class Student
+    {
+        [Index (0)]
+        public string Nome { get; set; } = "";
+        [Index(2)]
+        public string Codigo { get; set; } = "";
+        [Index(1)]
+        public string Email { get; set; } = "";
+
+    }
+}
